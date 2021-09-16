aa3525ebe2210b0435033433e474d3647a96c773
sachin <sachin@office>
2021-09-16 18:22:58 +0530

    initial code

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Nav from "../components/nav.svelte";
+</script>
+
+<Nav/>
+
+<slot />
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+  mode: 'jit',
+  purge: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+module.exports = config;
